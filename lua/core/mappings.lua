e46f64bdb3a392acf9c3e1fbd3f5b08859de1da8
BroNaz <brodnikovn@gmail.com>
2024-12-30 13:28:32 +0200

    add bufferline and neo-tree

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,9 +1,14 @@
 -- leader
 vim.g.mapleader = " "
 
--- buffer
+-- buffer.all
 vim.keymap.set('n', '<leader>q', ':q<CR>')
 vim.keymap.set('n', '<leader>w', ':w<CR>')
+-- buffer.bufferlines
+vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+
+
 
 -- insert
 vim.keymap.set('i', 'jj', '<Esc>')
@@ -15,4 +20,7 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.keymap.set('n', '|', ':vsplit<CR>')
 vim.keymap.set('n', '\\', ':split<CR>')
+
+-- neo-tree
+vim.keymap.set('n', '<leader>e', ':Neotree left toggle reveal<CR>')
 
