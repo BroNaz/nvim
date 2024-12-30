19118f1a366d662b5c3981241a378d7ade7173dc
BroNaz <brodnikovn@gmail.com>
2024-12-30 16:06:47 +0200

    add new keys

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -7,6 +7,8 @@ vim.keymap.set('n', '<leader>w', ':w<CR>')
 -- buffer.bufferlines
 vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+vim.keymap.set('n', '<c-x>', ':BufferLineCloseOthers<CR>')
+vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
 
 
 
