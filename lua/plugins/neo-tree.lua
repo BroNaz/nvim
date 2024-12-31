23288ad2b920e5e16cb6db4fdc7fe5ffa80dd71c
BroNaz <brodnikovn@gmail.com>
2024-12-31 14:15:03 +0200

    add trouble

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,6 +9,21 @@ return {
 			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
 		config = function()
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", fg = "none" })
+			vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = "none" })
+			
+			local icons = require "core.icons"
+			vim.diagnostic.config({
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = icons.diagnostics.Error,
+						[vim.diagnostic.severity.WARN] = icons.diagnostics.Warning,
+						[vim.diagnostic.severity.HINT] = icons.diagnostics.Hint,
+						[vim.diagnostic.severity.INFO] = icons.diagnostics.Information,
+					},
+				}
+			})
+
 			require("neo-tree").setup({
 				window = {
 					width = 30, -- замените 40 на нужное значение
